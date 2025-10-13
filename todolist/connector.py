fb8583622f71aa3829bfac1c0dcb27b24a88ef51
K-iemmei <phamkiem707@gmail.com>
2025-10-13 15:24:03 +0700

    basic todolist front end

---
+++ todolist/connector.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import sessionmaker 
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///C:/Users/phamx/OneDrive/Documents/Github/api-repo/todolist/todo_db.db"
+
+engine  = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit = False,
+    autoflush = False
+)
+Base = declarative_base() 
+
